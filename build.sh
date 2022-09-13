fe4243989812b964e899ec2d42040c6c14222528
OddRabbit <52036269+oddrabbit@users.noreply.github.com>
2022-09-13 18:57:04 +1000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+<< "7b567e913951468ff44cae31fd98d268"
+This program is free software: you can redistribute it and/or modify it under the terms of the 
+GNU General Public License as published by the Free Software Foundation, version 2 of the 
+License.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+General Public License for more details.
+You should have received a copy of the GNU General Public License along with this program. If not,
+see <https://www.gnu.org/licenses/>. 
+7b567e913951468ff44cae31fd98d268
+
+
 sudo docker build .
